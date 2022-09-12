fe620dc894aeeb9482d05f8d42b2016f171bcf7e
mrvinrsk <marvin.rosskothen@outlook.de>
2022-09-12 13:28:27 +0200

    begun seller page

--- verkaeufer-anzeigen.php
+++ verkaeufer-anzeigen.php
@@ -1,5 +1,10 @@
 <?php
 include_once "php/methods.php";
+
+$percent = rand(3, 20);
+$priceNormal = random_float(1.00, 20000.00);
+$priceReduced = number_format(getPercentageAmount($priceNormal, $percent), 2, ",", ".");
+$priceNormal = number_format($priceNormal, 2, ",", ".");
 ?>
 
 <!doctype html>
@@ -12,6 +17,8 @@ include_once "php/methods.php";
 
     <link rel="stylesheet" href="style/css/global.min.css">
     <link rel="stylesheet" href="style/css/home.min.css">
+    <link rel="stylesheet" href="style/css/artikel-anzeigen.min.css">
+    <link rel="stylesheet" href="style/css/verkaeufer-anzeigen.min.css">
 
     <script src="js/jquery-3.6.1.min.js"></script>
     <script src="js/jquery.waypoints.min.js"></script>
@@ -99,15 +106,58 @@ include_once "php/methods.php";
 </nav>
 
 <main>
-    <section>
-        <h2>Neuste Artikel</h2>
+    <section id="offer">
+        <a href="./index.php" class="icon-text large fc-primary no-underline hoverable">
+            <span class="icon">chevron_left</span>
+            <span>Zurück</span>
+        </a>
+
+        <div class="article-head">
+            <div class="article-main-image">
+                <img src="https://loremflickr.com/640/360" alt="Bild"/>
+            </div>
+
+            <div class="article-main-text">
+                <h1 class="article-name" data-placeholder="name"></h1>
+                <p class="article-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aliquam architecto, consequuntur debitis eos explicabo harum illum molestias nisi, nobis nostrum praesentium provident ratione sunt tempore! Est eum ullam voluptas?</p>
+
+                <div class="article-actions">
+                    <a class="button">Artikel</a>
+                    <a class="button">Bewertungen</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="buying-options">
+        <h2>Im Rampenlicht</h2>
+
+        <div class="featured-offer">
+            <img src="https://loremflickr.com/700/700?random=<?php echo rand(1, 99999); ?>" alt="Hervorgehobener Artikel: Bild"/>
+
+            <div class="information">
+                <div class="section-heading">
+                    <h3>Toller Artikel</h3>
+                    <span class="featured-rating stars"><?php echo getStarString(random_float(1., 5.), rand(10, 10000), true); ?></span>
+                </div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias asperiores eligendi illum ipsum iusto nam nesciunt, non praesentium ratione repellat sed tempore, temporibus vel vitae.</p>
+
+                <div class="actions">
+                    <p class="price"><?php echo number_format(random_float(100., 1000.), 2, ",", ".") . "€"; ?></p>
+                    <a href="artikel-anzeigen.php" class="button">Ansehen</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="also-bought">
+        <h2>Ähnliche Artikel</h2>
 
         <div class="article-grid">
-            <?php for ($i = 0; $i < rand(12, 24); $i++) { ?>
-
+            <?php for ($i = 0; $i < 4; $i++) { ?>
                 <div class="article">
                     <a href="artikel-anzeigen.php">
-                        <img src="https://loremflickr.com/175/175?random=<?php echo rand(1, 99999); ?>" alt="Bild"/>
+                        <img src="https://loremflickr.com/300/300?random=<?php echo rand(1, 99999); ?>" alt="Bild"/>
 
                         <div class="article-information">
                             <div class="text-group">
@@ -118,26 +168,46 @@ include_once "php/methods.php";
                         </div>
                     </a>
                 </div>
-
             <?php } ?>
         </div>
 
-        <div class="search">
-            <div class="section-heading">
-                <h2>Noch nicht das richtige dabei?</h2>
-                <p>Hat in letzter Zeit keinen Artikel online gestellt, der für dich relevant ist? Such hier einfach direkt was du brauchst!</p>
+    </section>
+
+    <section id="ratings">
+        <h2>Bewertungen</h2>
+
+        <div class="ratings-container">
+            <div class="all-time">
+                <div class="heading">
+                    <span class="icon">star</span>
+                    <p><?php echo number_format(random_float(1.0, 5.0), 1); ?>/5</p>
+                </div>
+
+                <p class="info">Diese Bewertung basiert auf den Ergebnissen von <b><?php echo rand(3, 50) . " Bewertungen"; ?></b>. Bitte beachte, dass positive Bewertungen nicht die Funktionalität eines Produktes garantieren.</p>
             </div>
-            <form class="input-group" action="search.php" method="GET">
-                <input type="text" name="search" id="search" placeholder="Suchen..."/>
-                <button>Suchen</button>
-            </form>
+
+            <div class="ratings">
+
+                <?php for ($i = 0; $i < 3; $i++) { ?>
+
+                    <div class="rating">
+                        <div class="profile">
+                            <img src="https://fakeimg.pl/200/" alt="Profilbild"/>
+                        </div>
+
+                        <div>
+                            <p class="heading"><span class="rating-count"><?php echo getStarString(random_float(1.0, 5.0), 1, false); ?></span><a class="review-user" href="nutzer-aktivitaet.html"><b data-placeholder="name"></b></a></p>
+                            <p class="rating-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti exercitationem libero odit sit ullam voluptate?</p>
+                        </div>
+                    </div>
+
+                <?php } ?>
+
+            </div>
         </div>
     </section>
 </main>
 
-<script>
-    loadPlaceholders();
-</script>
 
 <footer>
     <div>
@@ -153,6 +223,12 @@ include_once "php/methods.php";
         </div>
     </div>
 </footer>
+
+<script>
+    $(function () {
+        loadPlaceholders();
+    });
+</script>
 
 </body>
 </html>
