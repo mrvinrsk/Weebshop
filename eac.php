7955dc7ca84e4613fa89d1ebccf84f69bf427e5b
Marvin <marvin.rosskothen@outlook.de>
2022-10-02 20:26:40 +0200

    ne voll coole seite über eac

---
+++ eac.php
@@ -0,0 +1,77 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>EAC ist kagge</title>
+
+    <link rel="stylesheet" href="style/css/global.min.css">
+
+    <style>
+        .related-articles {
+            display: flex;
+            flex-direction: row;
+            gap: 1rem;
+        }
+
+        .rel {
+            max-width: 450px;
+            padding: 1rem;
+            border-radius: 10px;
+            border: 1px solid #888;
+        }
+
+        .related h2 {
+            margin-bottom: 1rem;
+        }
+
+        .rel {
+            font-size: .9rem;
+        }
+
+        .rel h3 {
+            font-size: 1em;
+        }
+
+        .button-yeah {
+            display: inline-block;
+            margin-top: .5rem;
+            text-decoration: none;
+            padding: .5em 1em;
+            background-color: #DEDEDE;
+            border-radius: 7px;
+            transition-duration: .25s;
+        }
+
+        .button-yeah:hover {
+            cursor: pointer;
+            background-color: #888;
+            color: white;
+        }
+    </style>
+</head>
+<body>
+
+<main>
+    <div>
+        <h1>Easy Anti-Cheat: Keine Cheat-Erkennung!?</h1>
+        <p>Ja, kein Wunder, dass es nicht funktioniert! Mensch, dat Ding muss aber kacke sein - unsere Recherche-Abteilung, welcher Sie auf Ehrenbruderbasis vertrauen können, hat herausgefunden, dass EAC tatsächlich nur prüft, ob Sie irgendeine Spieldatei geändert haben, so voll mit <strong><em>Rechtsklick > Eigenschaften</em></strong> und so, ey. Vallah, hätten wir das in unserer Redaktion programmiert wär das echt voll besser geworden!</p>
+    </div>
+
+    <div class="related">
+        <h2>Ähnliche Artikel</h2>
+
+        <div class="related-articles">
+            <div class="rel">
+                <h3>WhatsApp: Was sie bei einer Sprachnachricht bedenken müssen!</h3>
+                <p>Laut unseren neusten Forschungen ist es nicht möglich eine Sprachnachricht weiter aufzunehmen, wenn Sie die App verlassen, wat ein Scheiß!!</p>
+
+                <a class="button-yeah">Voll interessant und so, mehr lesen!</a>
+            </div>
+        </div>
+    </div>
+</main>
+
+</body>
+</html>
